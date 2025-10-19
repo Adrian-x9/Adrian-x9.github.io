c24c64ab3d3e579401f6c277572a7c51fc823d00
Adrian-x9 <adrian.ulbrych@gmail.com>
2025-10-19 17:26:35 +0200

    v2.4.15 Mobile view fixes / poprawki dla trybu mobilnego

--- ade-shared/js/config-shared.js
+++ ade-shared/js/config-shared.js
@@ -1,6 +1,6 @@
 /// Global configuration // Globalna konfiguracja
 const config1 = {
-  version: "v2.4.14", // carousel refactor / zajebista karuzela hehe
+  version: "v2.4.15", // Mobile view fixes / poprawki dla trybu mobilnego
   build: "2025-09-17", // Build date YYYY-MM-DD
   updated: "2025-10-19", // Last update date YYYY-MM-DD
 
