f998709cda3367d0c184903496db69d22ff1ab9a
Adrian-x9 <adrian.ulbrych@gmail.com>
2025-10-21 22:55:34 +0200

    fixes

---
+++ ade-subsystem/js/config-subsystem-mini.js
@@ -0,0 +1,87 @@
+const config2 = {
+  /// Subsystem specific settings // Ustawienia specyficzne dla podsystemu
+  pageSettings: {
+    pageKey: "indexMaxi",
+    pageTitle: "ade ▪ maxi MENU",
+    // pathCorrection: ".", // <-- NOWA LINIA: Definiuje korektę dla podsystemu
+    showMiniLogo: true,
+    showVisuDirButton: true,
+    showLanguageButton: true,
+    showBackButton: false,
+
+    screenSaverTimeout: 20,
+    defaultBgVideo: 1,
+    bgVideoStartNum: 1,
+    bgVideoEndNum: 76,
+    defaultVideoBg: true,
+    defaultLabelsVisible: true,
+    slideshowAnimations: true,
+
+    defaultSort: "nameAsc", //nameAsc | nameDesc | dateAsc | dateDesc | sizeAsc | sizeDesc | shuffle
+    defaultRows: 2,
+    defaultSizePercent: 66,
+    baseBoxWidth: 256,
+    estimatedItemHeight: 380,
+    defaultViewMode: "view-7",
+    hoverOverlayColor: "rgba(204, 0, 0, 0.7)",
+    hoverTextColor: "#ffffff",
+
+    defaultGeneratorMode: "visible",
+    defaultGeneratorCount: 5,
+    generatorOptions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 25, 50, 100],
+
+    defaultVideoExpanded: false,
+    predefinedFilters: [
+      {
+        label: "ADE",
+        search: "ade",
+      },
+      {
+        label: "E-booki",
+        search: "e-book",
+      },
+      {
+        label: "Galerie",
+        search: "galeri",
+      },
+      {
+        label: "Hiszpania",
+        search: "Hiszpania",
+      },
+      {
+        label: "Niemcy",
+        search: "Niemcy",
+      },
+      {
+        label: "Polska",
+        search: "Polska",
+      },
+      {
+        label: "Poradniki",
+        search: "Poradniki",
+      },
+    ],
+
+    defaultThemeFile: './ade-subsystem/css/styles-subsystem.css',
+  },
+  paths: {
+    /// Subsystem specific paths // Ścieżki specyficzne dla podsystemu
+    databaseFileName: "./ade-subsystem/ade-tools/.db-secure.txt",
+    dataFolderName: "./ade-subsystem/files",
+    coversFolderName: "./ade-subsystem/preview",
+    previewHdFolderName: "./ade-subsystem/preview-hd",
+  },
+};
+
+const config = {
+  ...config1,
+  pageSettings: {
+    ...config1.pageSettings,
+    ...config2.pageSettings,
+    ...config0.pageSettings,
+  },
+  paths: {
+    ...config1.paths,
+    ...config2.paths,
+  },
+};
--- ade-subsystem/js/db-mini.js
+++ ade-subsystem/js/db-mini.js
@@ -1,15 +1,7 @@
 /// COPY HERE <db> // database - txt file / baza danych - plik txt
         const dataFromFile = `
-17.09.2025  20:27                 0 github.com!!Adrian-x9$$38 Nasz GitHub$$Autorskie projekty na GitHub.link
+17.09.2025  18:40                 0 _!!index-maxi.html$$11 maxi MENU$$Otworz maxi MENU.link
 17.09.2025  20:19                 0 _!!ade-shared!!about.html$$31 VisuDir$$Oprogramowanie do wizualizacji zawartosci katalogow.link
-17.09.2025  19:00                 0 _!!ade-shared!!author.html$$17 Autor$$Adrian Ulbrych. Informacje o autorze.link
-17.09.2025  19:00                 0 _!!ade-shared!!contact.html$$15 Kontakt$$Masz pytania, uwagi - skontaktuj sie!.link
-17.09.2025  20:21                 0 _!!adeDesk!!index.html$$33 ade Desk$$Oprogramowanie do pracy zdalnej i nadzoru.link
-17.09.2025  20:26                 0 _!!adeINFO!!index.html$$35 ade INFO$$Twoje IP i kilka dodatkowych informacji.link
-17.09.2025  20:26                 0 _!!adePages!!index.html$$36 ade Pages$$Projekty witryn internetowych.link
-17.09.2025  20:26                 0 _!!adeSync!!index.html$$34 ade Sync$$System do codziennych kopii zapasowych.link
-17.09.2025  22:51                 0 _!!AI!!index.html$$71 AI$$71 Informacje dotyczace Sztucznej Inteligencji, AI.link
-17.09.2025  19:59                 0 _!!Certificates!!index.html$$37 Certyfikaty$$Zobacz nasze certyfikaty.link
 17.09.2025  22:50                 0 _!!Cyprus!!index.html$$66 Cypr$$Galeria fotografii z Cypru.link
 17.09.2025  22:50                 0 _!!Czechia!!index.html$$60 Czechy$$Galeria fotografii z Czech.link
 17.09.2025  22:50                 0 _!!Czechia!!index.html&&search=Ostrava$$61 Ostrava$$Galeria fotografii z Ostravy, Czechy.link
@@ -22,11 +14,7 @@
 17.09.2025  22:10                 0 _!!Germany!!index.html$$57 Niemcy$$Galeria fotografii z Niemiec.link
 17.09.2025  22:50                 0 _!!Germany!!index.html&&search=Berlin$$59 Berlin$$Galeria fotografii z Berlina.link
 17.09.2025  22:49                 0 _!!Germany!!index.html&&search=Dortmund$$58 Dortmund$$Galeria fotografii z Dortmundu.link
-17.09.2025  18:41                 0 _!!index-midi.html$$12 midi MENU$$Otworz srednie MENU.link
-17.09.2025  19:57                 0 _!!index.html$$13 mini MENU$$Otworz MENU minimalne.link
-17.09.2025  19:58                 0 _!!index-portfolio.html$$16 Portfolio$$Obejrzyj nasze portfolio.link
 17.09.2025  20:37                 0 _!!index-travel.html$$51 Podroze$$Dzialanie VisuDir na przykladzie foto galerii.link
-17.09.2025  18:40                 0 _!!index.html$$11 maxi MENU$$Otworz MENU standardowe.link
 17.09.2025  21:41                 0 _!!Poland!!index.html$$52 Polska$$Galeria fotografii z Polski.link
 17.09.2025  21:47                 0 _!!Poland!!index.html&&search=Gliwice$$53 Gliwice$$Galeria fotografii z Gliwic, Slask, Silesia.link
 17.09.2025  22:10                 0 _!!Poland!!index.html&&search=Morze$$56 Morze Baltyckie$$Galeria fotografii Morza Baltyckiego, Baltic Sea.link
