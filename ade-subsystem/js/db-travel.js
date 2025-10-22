e622433a799ce230000b14ff7cc4f2c9c8c25d0f
Adrian-x9 <adrian.ulbrych@gmail.com>
2025-10-22 20:57:04 +0200

    fixes

---
+++ ade-subsystem/js/db-travel.js
@@ -0,0 +1,22 @@
+/// COPY HERE <db> // database - txt file / baza danych - plik txt
+        const dataFromFile = `
+17.09.2025  20:37                 0 _!!.!!index.html$$11 START$$Wróć na początek.link
+17.09.2025  21:41                 0 _!!Poland!!index.html$$31 Polska$$Galeria fotografii z Polski.link
+17.09.2025  22:10                 0 _!!Poland!!index.html&&search=Warszawa$$32 Warszawa$$Galeria fotografii z Warszaway, Mazowsze, Mazovia.link
+17.09.2025  21:47                 0 _!!Poland!!index.html&&search=Gliwice$$33 Gliwice$$Galeria fotografii z Gliwic, Slask, Silesia.link
+17.09.2025  22:10                 0 _!!Poland!!index.html&&search=Gory$$34 Polskie góry$$Galeria fotografii polskic gór.link
+17.09.2025  22:10                 0 _!!Poland!!index.html&&search=Morze$$35 Morze Baltyckie$$Galeria fotografii Morza Baltyckiego, Baltic Sea.link
+17.09.2025  22:10                 0 _!!Germany!!index.html$$41 Niemcy$$Galeria fotografii z Niemiec.link
+17.09.2025  22:50                 0 _!!Germany!!index.html&&search=Berlin$$42 Berlin$$Galeria fotografii z Berlina.link
+17.09.2025  22:49                 0 _!!Germany!!index.html&&search=Dortmund$$43 Dortmund$$Galeria fotografii z Dortmundu.link
+17.09.2025  22:50                 0 _!!Czechia!!index.html$$51 Czechy$$Galeria fotografii z Czech.link
+17.09.2025  22:50                 0 _!!Czechia!!index.html&&search=Praga$$52 Praga$$Galeria fotografii z Pragi, Czechy.link
+17.09.2025  22:50                 0 _!!Czechia!!index.html&&search=Ostrava$$53 Ostrava$$Galeria fotografii z Ostravy, Czechy.link
+17.09.2025  22:50                 0 _!!Spain!!index.html$$61 Hiszpania$$61 Galeria fotografii z Hiszpanii.link
+17.09.2025  22:50                 0 _!!Spain!!index.html&&search=Madrid$$62 Madrid$$81 Galeria fotografii z Madrytu.link
+17.09.2025  22:50                 0 _!!Spain!!index.html&&search=Barcelona$$63 Barcelona$$Galeria fotografii z Barcelony, Hiszpania.link
+17.09.2025  22:50                 0 _!!Spain!!index.html&&search=LLoret$$64 Lloret de Mar$$Galeria fotografii z Lloret de Mar, Hiszpania.link
+17.09.2025  22:50                 0 _!!Cyprus!!index.html$$69 Cypr$$Galeria fotografii z Cypru.link
+
+        `;
+        /// COPY HERE <db> // database end / koniec bazy danych
