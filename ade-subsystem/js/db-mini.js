bd5efde4837e1e24daf420ac34d37755810b047b
Adrian-x9 <adrian.ulbrych@gmail.com>
2025-10-23 11:46:05 +0200

    fixes

--- ade-subsystem/js/db-mini.js
+++ ade-subsystem/js/db-mini.js
@@ -1,6 +1,9 @@
 /// COPY HERE <db> // database - txt file / baza danych - plik txt
         const dataFromFile = `
-17.09.2025  20:37                 0 _!!index-travel.html$$21 Podroze$$Dzialanie VisuDir na przykladzie foto galerii.link
+23.10.2025  23:59                 0 _!!index-projects.html$$11 Nasze projekty$$VisuDir, VisuKeys, adeDesk, adeSync i inne.link
+23.10.2025  23:59                 0 _!!index-blog.html$$12 BLOG$$BLOG i artykuły na różne tamaty.link
+23.10.2025  23:59                 0 _!!index-info.html$$13 Kontakt i informacje$$Dane kontaktowe i inne informacje.link
+17.09.2025  20:37                 0 _!!index-travel.html$$21 Podróże$$Działanie VisuDir na przykładzie foto galerii.link
 17.09.2025  21:41                 0 _!!Poland!!index.html$$31 Polska$$Galeria fotografii z Polski.link
 17.09.2025  22:10                 0 _!!Germany!!index.html$$41 Niemcy$$Galeria fotografii z Niemiec.link
 17.09.2025  22:50                 0 _!!Czechia!!index.html$$51 Czechy$$Galeria fotografii z Czech.link
